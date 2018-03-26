9557776b256688c9f161e21a305f75402d8a2814
wengshian1994 <wengshih@andrew.cmu.edu>
2018-03-26 10:55:31 -0400

    integrated MCA into strategy file

--- midwest_trading_2018-master/midwest_cmu_strategy.py
+++ midwest_trading_2018-master/midwest_cmu_strategy.py
@@ -22,8 +22,9 @@ class SampleStrategy(PortfolioGenerator):
         self.stock_indices = np.load("preloaded_data/stock_indices.npy")
         self.buffer_size = 100 #look back period
         #self.ticker_data = self.create_buffer_data()
-        # number of simulations
+        # number of simulations for markowitz
         self.num_sims = 10000
+        self.to_use_MCA = True #implement MCA
     
     def create_buffer_data(self):
         ticker_data = pd.read_csv('original_stock_data/ticker_data.csv')
@@ -39,6 +40,7 @@ class SampleStrategy(PortfolioGenerator):
         return ticker_df
     
     def MCA(self, stock_data):
+        global p
         # p - Pearson Matrix (correlation matrix)
         p = np.corrcoef(np.transpose(stock_data))
 
@@ -74,29 +76,31 @@ class SampleStrategy(PortfolioGenerator):
         selected_ticker_df = ticker_df[list(self.stock_indices)]
         cov_mat = np.cov(selected_ticker_df,rowvar=False)
         #random mu (before LSTM implementation)
+        if self.to_use_MCA==True:
+            all_weights = self.MCA(selected_ticker_df)
         
-        
-        mean_vec = np.ones(len(self.stock_indices))/100
-        sim_dataframe = pd.DataFrame(columns=["Weights","Sharpe"])
-        for i in range(self.num_sims):
-            weights = np.random.uniform(low=-1,high=1,size=len(self.stock_indices))
-            #normalize weights
-            weights = weights/np.std(weights)
-            w_t_dot_cov_mat = np.reshape(np.dot(weights.transpose(),cov_mat),(1,300))
-            sigma = math.sqrt(np.dot(w_t_dot_cov_mat,weights))
-            #print(sigma)
-            mu = np.dot(weights,mean_vec)
-            sharpe_sample = mu/sigma
-            sim_dataframe.at[i,"Weights"]=weights
-            sim_dataframe.at[i,"Sharpe"]=sharpe_sample
-        
-        max_weights = sim_dataframe.loc[np.array(sim_dataframe["Sharpe"]).argmax()]["Weights"]
-        
-        #need to piece back max weights into a 1000x1 vector
-        all_weights = np.zeros(1000)
-        for entry in range(len(self.stock_indices)):
-            ref_index = self.stock_indices[entry]
-            all_weights[ref_index] = max_weights[entry] 
+        else:
+            mean_vec = np.ones(len(self.stock_indices))/100
+            sim_dataframe = pd.DataFrame(columns=["Weights","Sharpe"])
+            for i in range(self.num_sims):
+                weights = np.random.uniform(low=-1,high=1,size=len(self.stock_indices))
+                #normalize weights
+                weights = weights/np.std(weights)
+                w_t_dot_cov_mat = np.reshape(np.dot(weights.transpose(),cov_mat),(1,300))
+                sigma = math.sqrt(np.dot(w_t_dot_cov_mat,weights))
+                #print(sigma)
+                mu = np.dot(weights,mean_vec)
+                sharpe_sample = mu/sigma
+                sim_dataframe.at[i,"Weights"]=weights
+                sim_dataframe.at[i,"Sharpe"]=sharpe_sample
+            
+            max_weights = sim_dataframe.loc[np.array(sim_dataframe["Sharpe"]).argmax()]["Weights"]
+            
+            #need to piece back max weights into a 1000x1 vector
+            all_weights = np.zeros(1000)
+            for entry in range(len(self.stock_indices)):
+                ref_index = self.stock_indices[entry]
+                all_weights[ref_index] = max_weights[entry] 
             
         return all_weights
 
