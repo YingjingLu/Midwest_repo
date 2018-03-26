17c45d9c7b8669e4abfe8fcac92cba7a49090180
wengshian1994 <wengshih@andrew.cmu.edu>
2018-03-26 00:21:02 -0400

    finishing up build_signal

--- midwest_trading_2018-master/midwest_cmu_strategy.py
+++ midwest_trading_2018-master/midwest_cmu_strategy.py
@@ -9,16 +9,18 @@ Created on Sun Mar 25 22:33:59 2018
 from portfolio import PortfolioGenerator
 import pandas as pd
 import numpy as np
-
+import math 
 class SampleStrategy(PortfolioGenerator):
 
     def __init__(self):
         #insert model weights in here (Neural Net weights)
         
         #stock indices are loaded here 
-        self.stock_indicies = np.load("preloaded_data/stock_indices.npy")
+        self.stock_indices = np.load("preloaded_data/stock_indices.npy")
         self.buffer_size = 100 #look back period
         self.ticker_data = self.create_buffer_data()
+        # number of simulations
+        self.num_sims = 1000
     
     def create_buffer_data(self,stock_features):
         ticker_data = pd.read_csv('original_stock_data/ticker_data.csv')
@@ -34,12 +36,29 @@ class SampleStrategy(PortfolioGenerator):
         return ticker_df
         
     def build_signal(self, stock_features):
+        np.set
         ticker_df = shape_data(stock_features)
+        selected_ticker_df = ticker_df[list(self.stock_indices)]
+        cov_mat = np.cov(selected_ticker_df,rowvar=False)
+        #random mu (before LSTM implementation)
+        mean_vec = np.ones(len(stock_indices))/100
+        sim_dataframe = pd.DataFrame(columns=["Weights","Sharpe"])
+        for i in range(self.num_sims):
+            weights = np.random.uniform(low=-1,high=1,size=len(portfolio.stock_indicies))
+            #normalize weights
+            weights = weights/np.std(weights)
+            sigma = math.sqrt(np.dot(np.dot(weights,cov_mat),weights.transpose()))
+            mu = np.dot(weights,mean_vec)
+            sharpe_sample = mu/sigma
+            sim_dataframe[i,"Weights"]=weights
+            sim_dataframe[i,"Sharpe"]=sharpe_sample
+            
+            
+            
         
         return self.momentum(stock_features)
 
-    def momentum(self, stock_features):
-        return stock_features.groupby(['ticker'])['returns'].mean()
+
 
 # Test out performance by running 'python sample_strategy.py'
 if __name__ == "__main__":
